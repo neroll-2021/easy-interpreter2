3e9eb655fba42a802e87f4cd3d70ffce36def0e9
Neroll <1343809614way@gmail.com>
2024-11-01 15:11:43 +0800

    完成 lexer::next_token 的主要功能，修复 token_type 和 token 无法正确格式化的 bug

--- include/detail/lexer.h
+++ include/detail/lexer.h
@@ -1,12 +1,15 @@
 #ifndef NEROLL_SCRIPT_DETAIL_LEXER_H
 #define NEROLL_SCRIPT_DETAIL_LEXER_H
 
+#include <print>
 #include <string>           // string
 #include <cstddef>          // size_t
 #include <format>           // formatter
+#include <string_view>      // string_view
 #include <unordered_map>    // unordered_map
 #include <cassert>          // assert
 
+#include "exception.h"
 #include "input_adapter.h"
 #include "position_t.h"
 
@@ -41,6 +44,8 @@ enum class token_type {
     literal_false,      // false
     literal_string,     // "string"
     literal_char,       // 'c'
+
+    identifier,
 
     plus,               // +
     minus,              // -
@@ -98,7 +103,10 @@ struct token {
     token() = default;
 
     token(token_type type_, const position_t &position)
-        : type(type_), line(position.lines_read + 1),
+        : token("", type_, position) {}
+    
+    token(std::string_view content_, token_type type_, const position_t &position)
+        : content(content_), type(type_), line(position.lines_read + 1),
           column(position.chars_read_current_line) {}
 };
 
@@ -142,6 +150,8 @@ const char *token_type_name(token_type type) {
             return "literal string";
         case token_type::literal_char:
             return "literal char";
+        case token_type::identifier:
+            return "identifier";
         case token_type::plus:
             return "+";
         case token_type::minus:
@@ -228,7 +238,109 @@ class lexer {
         : adapter_(std::move(adapter)) {}
 
     token next_token() {
-        return {};
+        skip_whitespace();
+
+        switch (current_) {
+            case '+':
+                return token{"+", token_type::plus, position_};
+            case '-':
+                return token{"-", token_type::minus, position_};
+            case '*':
+                return token{"*", token_type::asterisk, position_};
+            case '/':
+                return token{"/", token_type::slash, position_};
+            case '%':
+                return token{"%", token_type::mod, position_};
+            case '&':
+                if (get() == '&')
+                    return token{"&&", token_type::logical_and, position_};
+                unget();
+                return token{"&", token_type::bit_and, position_};
+            case '|':
+                if (get() == '|')
+                    return token{"||", token_type::logical_or, position_};
+                unget();
+                return token{"|", token_type::bit_or, position_};
+            case '^':
+                return token{"^", token_type::bit_xor, position_};
+            case '~':
+                return token{"~", token_type::bit_not, position_};
+            case '<': {
+                char_int_type next = get();
+                if (next == '<')
+                    return token{"<<", token_type::shift_left, position_};
+                if (next == '=')
+                    return token{"<=", token_type::less_equal, position_};
+                unget();
+                return token{"<", token_type::less, position_};
+            }
+            case '>': {
+                char_int_type next = get();
+                if (next == '>')
+                    return token{">>", token_type::shift_right, position_};
+                if (next == '=')
+                    return token{">=", token_type::greater_equal, position_};
+                unget();
+                return token{">", token_type::greater, position_};
+            }
+            case '\\':
+                return token{"\\", token_type::backslash, position_};
+            case '!':
+                if (get() == '=')
+                    return token{"!=", token_type::not_equal, position_};
+                unget();
+                return token{"!", token_type::logical_not, position_};
+            case '=':
+                if (get() == '=')
+                    return token{"==", token_type::equal, position_};
+                unget();
+                return token{"=", token_type::assign, position_};
+            case ';':
+                return token{";", token_type::semicolon, position_};
+            case ':':
+                return token{":", token_type::colon, position_};
+            case ',':
+                return token{",", token_type::comma, position_};
+            case '.':
+                return token{".", token_type::dot, position_};
+            case '(':
+                return token{"(", token_type::left_parenthesis, position_};
+            case ')':
+                return token{")", token_type::right_parenthesis, position_};
+            case '[':
+                return token{"[", token_type::left_bracket, position_};
+            case ']':
+                return token{"]", token_type::right_bracket, position_};
+            case '{':
+                return token{"{", token_type::left_brace, position_};
+            case '}':
+                return token{"}", token_type::right_brace, position_};
+            case '\'':
+                return token{"'", token_type::single_quotation, position_};
+            case '"':
+                return token{"\"", token_type::double_quotation, position_};
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                return scan_number();
+            case '\0':
+            case std::char_traits<char_type>::eof():
+                return token{"eof", token_type::end_of_input, position_};
+            default:
+                if (std::isalpha(current_) || current_ == '_') {
+                    return scan_identifier();
+                }
+                throw_syntax_error("line {}, column {}: unknown token",
+                    position_.lines_read + 1, position_.chars_read_current_line
+                );
+        }
     }
 
     void rewind() {
@@ -241,7 +353,7 @@ class lexer {
     bool next_unget_ = false;
     char_int_type current_ = std::char_traits<char_type>::eof();
     std::string token_string_;
-    const static inline std::unordered_map<std::string_view, token_type> keywords = {
+    const static inline std::unordered_map<std::string_view, token_type> keywords_ = {
         {"int", token_type::keyword_int}, {"float", token_type::keyword_float},
         {"boolean", token_type::keyword_boolean}, {"string", token_type::keyword_string},
         {"char", token_type::keyword_char}, {"function", token_type::keyword_function},
@@ -250,6 +362,26 @@ class lexer {
         {"continue", token_type::keyword_continue}, {"break", token_type::keyword_break},
         {"return", token_type::keyword_return}
     };
+
+    token scan_number() {
+        return token{"123", token_type::literal_int, position_};
+    }
+    
+    token scan_identifier() {
+        reset();
+        while (std::isalnum(current_) || current_ == '_') {
+            get();
+        }
+        unget();
+        auto iter = keywords_.find(token_string_);
+        if (iter != keywords_.end())
+            return {iter->first, iter->second, position_};
+        if (token_string_ == "true")
+            return token{token_string_, token_type::literal_true, position_};
+        if (token_string_ == "false")
+            return token{token_string_, token_type::literal_false, position_};
+        return token{token_string_, token_type::identifier, position_};
+    }
 
     char_int_type get() {
         ++position_.chars_read_total;
@@ -319,9 +451,11 @@ class lexer {
 template <>
 struct std::formatter<neroll::script::detail::token_type> {
     constexpr auto parse(std::format_parse_context &context) {
-        if (context.begin() != context.end())
+        auto it = context.begin();
+        if (it != context.end() && *it != '}') {
             throw std::format_error("invalid format specifier");
-        return context.begin();
+        }
+        return it;
     }
 
     auto format(neroll::script::detail::token_type type, std::format_context &context) const {
@@ -333,19 +467,15 @@ struct std::formatter<neroll::script::detail::token_type> {
 template <>
 struct std::formatter<neroll::script::detail::token> {
     constexpr auto parse(std::format_parse_context &context) {
-        if (context.begin() != context.end())
+        auto it = context.begin();
+        if (it != context.end() && *it != '}') {
             throw std::format_error("invalid format specifier");
-        return context.begin();
+        }
+        return it;
     }
 
     auto format(const neroll::script::detail::token &token, std::format_context &context) const {
         using neroll::script::detail::token_type_name;
-        using neroll::script::detail::token_type;
-
-        if (token.type == token_type::end_of_input)
-            return std::format_to(context.out(), "<EOF, {}>", token_type_name(token.type));
-        if (token.type == token_type::parse_error)
-            return std::format_to(context.out(), "<parse error, {}>", token_type_name(token.type));
         return std::format_to(context.out(), "<{}, {}>", token.content, token_type_name(token.type));
     }
 };
